d5f849f5b6611c4d891af1be155c21dcdfdd1cb0
Justin Berger Howes <howes.j.j@gmail.com>
2021-08-31 19:11:18 +1000

    artist complete

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        songs = Song.all
+        songs.select{|song| song.artist == self}
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def self.find_or_create_by_name(name)
+        # self.all.include?(name) ? self.all.find{|artist| artist.name = name} : self.new(name)
+        self.all.find{|artist| artist.name = name} == nil ? self.new(name) : self.all.find{|artist| artist.name = name}
+    end
+end
